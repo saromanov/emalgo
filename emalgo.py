d399ce9debcd72bba6a116225abcbfae4fe49a3d
Sergey Romanov <xxsmotur@gmail.com>
2015-10-22 22:22:22 +0500

    First commit

---
+++ emalgo.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+class EM:
+    def __init__(self, X1, X2):
+        self.X1 = X1
+        self.X2 = X2
+
+    def fit(samples=5, n=10, eps=0.005):
+        theta1 = 0.5
+        theta2 = 0.6
+        for i in range(samples):
+
+            sample1 = np.random.binomial(p=0.5, n=n)
+            sample2 = np.random.binomial(p=0.5, n=n)
+            H1, T1 = self._ML(sample1, n)
+            H2, T2 = self._ML(sample2, n)
+            value1 = theta1 * H1
+            value2 = theta1 * T1
+            value3 = theta2 * H2
+            value4 = theta2 * T2
+
+            theta1_new = np.sum(value1)/(np.sum(value1) + np.sum(value2))
+            theta2_new = np.sum(value3)/(np.sum(value3) + np.sum(value4))
+            print("Error: ", np.abs(theta1_new - theta1)/theta1, np.abs(theta2_new - theta2)/theta2)
+            theta1, theta2 = theta1_new, theta2_new
+
+    def _ML(self, item, size):
+        H = np.where(item == 1)
+        return len(H), size - len(H)
+
+
