ae9bc924b9f012dc0598dfa42bbd9f1675cb8af5
Sergey Romanov <xxsmotur@gmail.com>
2015-10-30 22:40:10 +0500

    Add logging

--- emalgo.py
+++ emalgo.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import logging
 
 class EM:
     def __init__(self, X1, X2):
@@ -9,10 +9,11 @@ class EM:
     def fit(self, samples=5, n=10, eps=0.005):
         theta1 = 0.5
         theta2 = 0.6
+        for
         for i in range(samples):
 
-            sample1 = np.random.binomial(p=self.X1[i], n=n)
-            sample2 = np.random.binomial(p=self.X2[i], n=n)
+            sample1 = np.random.binomial(self.X1[i], theta1, n)
+            sample2 = np.random.binomial(self.X2[i], theta2, n)
             H1, T1 = self._ML(sample1, n)
             H2, T2 = self._ML(sample2, n)
             value1 = theta1 * H1
@@ -22,7 +23,7 @@ class EM:
 
             theta1_new = np.sum(value1)/(np.sum(value1) + np.sum(value2))
             theta2_new = np.sum(value3)/(np.sum(value3) + np.sum(value4))
-            print("Error: ", np.abs(theta1_new - theta1)/theta1, np.abs(theta2_new - theta2)/theta2)
+            logging.info("Error: {0}".format(np.abs(theta1_new - theta1)/theta1, np.abs(theta2_new - theta2)/theta2))
             theta1, theta2 = theta1_new, theta2_new
 
     def _ML(self, item, size):
